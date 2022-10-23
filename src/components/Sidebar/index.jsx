b597195513d13444f0bbd552bcf3d9c8f45d93d5
dandiindra29 <dandiindra29@gmail.com>
2022-10-23 08:34:31 +0700

    sidebar finish

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,26 +1,62 @@
 import React, { useEffect } from "react"
+import Chevron from "../../icons/chevron"
 import './Sidebar.css'
 
 const Sidebar = (props) => {
-  const { isOpen } = props
+  const { isOpen, toggleSidebarButton } = props
 
   useEffect(() => {
     const sidebar = document.getElementById('sidebar')
-    const sidebarItemsContainer = document.getElementById("sidebar-items-container")
+    const sidebarItemsContainer = document.getElementById("sidebar-items")
     if(isOpen){
+      window.scrollTo(0, 0);
+      document.body.style.overflowY = 'hidden'
       sidebar.classList.add('show-sidebar')
       sidebarItemsContainer.classList.add('sidebar-open')
     }else {
+      document.body.style.overflowY = 'scroll'
       sidebar.classList.remove('show-sidebar')
       sidebarItemsContainer.classList.remove('sidebar-open')
     }
   }, [isOpen])
+
+  const onClickBlankSidebar = () => {
+    toggleSidebarButton()
+    const sidebar = document.getElementById('sidebar')
+    const sidebarItemsContainer = document.getElementById("sidebar-items")
+    // document.body.style.overflow = 'scroll'
+    sidebar.classList.remove('show-sidebar')
+    sidebarItemsContainer.classList.remove('sidebar-open')
+  }
   
   return (
     <div id="sidebar" className="sidebar">
-     <div id="sidebar-items-container" className="sidebar-items-container sidebar-open">
-          d
+      <div id="blank-sidebar" onClick={onClickBlankSidebar}></div>
+      <div id="sidebar-items" className="sidebar-items sidebar-open">
+        <div className="sidebar-items-container">
+          <div className="sidebar-link">
+            <span className="sidebar-link-title">Menu</span> <Chevron style={{ width: '32px', height: '32px', transform: 'rotate(-90deg)' }} />
+          </div>
+          <div className="sidebar-link">
+            Rewards
+          </div>
+          <div className="sidebar-link">
+            Gift Cards
+          </div>
+          <div className="divider"></div>
+          <div className="sidebar-sign-button-container">
+              <button className="sign-in sidebar-sign-button">
+                Sign in
+              </button>
+              <button className="join-now sidebar-join-button">
+                Join now
+              </button>
+          </div>
+          <div className="find-store">
+            <svg aria-hidden="true" className="maps-icon" focusable="false" preserveAspectRatio="xMidYMid meet" style={{width:'24px',height:'24px',overflow:'visible',fill:'currentColor'}} viewBox="0 0 24 24"><path d="M12,11.475 C10.5214286,11.475 9.32142857,10.299 9.32142857,8.85 C9.32142857,7.401 10.5214286,6.225 12,6.225 C13.4785714,6.225 14.6785714,7.401 14.6785714,8.85 C14.6785714,10.299 13.4785714,11.475 12,11.475 M12,1.5 C7.85357143,1.5 4.5,4.7865 4.5,8.85 C4.5,14.3625 12,22.5 12,22.5 C12,22.5 19.5,14.3625 19.5,8.85 C19.5,4.7865 16.1464286,1.5 12,1.5"></path></svg> <span className="text">Find a store</span>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
